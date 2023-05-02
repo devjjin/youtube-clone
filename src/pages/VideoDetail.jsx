06ca0e0a2c06458e0638cc6d5139d65989bf2e5d
devjjin <sujink34@naver.com>
2023-05-02 18:53:58 +0900

    Feat: iFrame API로 영상 해당 id 영상 가져오기

--- src/pages/VideoDetail.jsx
+++ src/pages/VideoDetail.jsx
@@ -1,5 +1,23 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 
 export default function VideoDetail() {
-  return <div>Video Detail</div>;
+  const {
+    state: { video },
+  } = useLocation();
+  return (
+    <section>
+      <article>
+        <iframe
+          title='title'
+          id='player'
+          type='text/html'
+          width='100%'
+          height='640'
+          src={`http://www.youtube.com/embed/${video.id}`}
+          frameborder='0'
+        />
+      </article>
+    </section>
+  );
 }
