6cca8fb50f88a5af47a086e6031e51afc6490e05
devjjin <sujink34@naver.com>
2023-05-02 21:46:29 +0900

    Design: 상세정보 컴포넌트 스타일링

--- src/pages/VideoDetail.jsx
+++ src/pages/VideoDetail.jsx
@@ -9,10 +9,10 @@ export default function VideoDetail() {
   } = useLocation();
   const { title, channelId, channelTitle, description } = video.snippet;
   return (
-    <section>
-      <article>
+    <section className='flex flex-col lg:flex-row'>
+      <article className='basis-4/6'>
         <iframe
-          title='title'
+          title={title}
           id='player'
           type='text/html'
           width='100%'
@@ -20,16 +20,16 @@ export default function VideoDetail() {
           src={`http://www.youtube.com/embed/${video.id}`}
           frameborder='0'
         />
-        <div>
-          <h1>{title}</h1>
+        <div className='p-8'>
+          <h2 className='text-xl font-bold'>{title}</h2>
           {/* chaneelId를 전달해서 cahnnel정보를 api를 통해 받아와야한다. */}
           <ChannelInfo id={channelId} name={channelTitle} />
           {/* 여백,공백,space적절히 조절해서 렌더링 */}
-          <pre>{description}</pre>
+          <pre className='whitespace-pre-wrap'>{description}</pre>
         </div>
       </article>
       {/* 연관된 비디오 보여주는 별도 컴포넌트 */}
-      <section>
+      <section className='basis-2/6'>
       <RelatedVideos id={video.id} />
       </section>
     </section>
