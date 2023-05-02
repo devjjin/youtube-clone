2a7f7453c706f44246f7bd3384e54a5ce3e0009a
devjjin <sujink34@naver.com>
2023-05-02 21:55:36 +0900

    Feat: 캐싱전략을 위해 staleTime 조정

--- src/context/YoutubeApiContext.jsx
+++ src/context/YoutubeApiContext.jsx
@@ -1,13 +1,13 @@
 import { createContext, useContext } from 'react';
 import Youtube from '../api/youtube';
 import YoutubeClient from '../api/youtubeClient';
-import FakeYoutubeClient from '../api/fakeYoutubeClient';
+// import FakeYoutubeClient from '../api/fakeYoutubeClient';
 
 export const YoutubeApiContext = createContext();
 
 // mock데이터,실제api client를 스위칭하면서 테스트
-const client = new FakeYoutubeClient();
-// const client = new YoutubeClient();
+// const client = new FakeYoutubeClient();
+const client = new YoutubeClient();
 const youtube = new Youtube(client);
 
 export function YoutubeApiProvider({ children }) {
