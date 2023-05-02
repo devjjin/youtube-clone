319bed37fb4f18a7113b5aa17da9358eeec84d41
devjjin <sujink34@naver.com>
2023-05-02 18:57:32 +0900

    Feat: 연관된 동영상 컴포넌트 추가 및 골격

--- src/pages/VideoDetail.jsx
+++ src/pages/VideoDetail.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
 import ChannelInfo from '../components/ChannelInfo';
+import RelatedVideos from '../components/RelatedVideos';
 
 export default function VideoDetail() {
   const {
@@ -21,11 +22,16 @@ export default function VideoDetail() {
         />
         <div>
           <h1>{title}</h1>
-          {/* Chaneel정보 컴포넌트 */}
+          {/* chaneelId를 전달해서 cahnnel정보를 api를 통해 받아와야한다. */}
           <ChannelInfo id={channelId} name={channelTitle} />
+          {/* 여백,공백,space적절히 조절해서 렌더링 */}
           <pre>{description}</pre>
         </div>
       </article>
+      {/* 연관된 비디오 보여주는 별도 컴포넌트 */}
+      <section>
+      <RelatedVideos id={video.id} />
+      </section>
     </section>
   );
 }
