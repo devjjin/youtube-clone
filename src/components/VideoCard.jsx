8e87982a024d18441540fdaa77f49c3faf89a3fe
devjjin <sujink34@naver.com>
2023-05-02 17:45:33 +0900

    Feat: 비디오 카드 상세 내용 보여주는 기능

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,6 +1,18 @@
 import React from 'react';
+import {formatAgo} from '../util/date';
 
 export default function VideoCard({ video }) {
-  return <div>{video.snippet.title}</div>;
-  // popuplar.json 참고
+  const { title, thumbnails, channelTitle, publishedAt } = video.snippet;
+  return (
+    <li>
+      <img src={thumbnails.medium.url} alt={title} />
+      <div>
+        <p>{title}</p>
+        <p>{channelTitle}</p>
+        <p>{formatAgo(publishedAt)}</p>
+        {/* 한국어 표기 */}
+        {/* <p>{formatAgo(publishedAt, 'ko')}</p> */} 
+      </div>
+    </li>
+  );
 }
